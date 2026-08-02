208dfba6560ae30f3d424476571a99ca9ce5c43d
kanishka-gupta1 <kanishkagupta428@gmail.com>
2026-08-02 23:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.59%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+
+        if not nums:
+            return result
+
+        start = nums[0]
+        end = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == end + 1:
+                end = nums[i]
+            else:
+                if start == end:
+                    result.append(str(start))
+                else:
+                    result.append(f"{start}->{end}")
+
+                start = nums[i]
+                end = nums[i]
+
+        if start == end:
+            result.append(str(start))
+        else:
+            result.append(f"{start}->{end}")
+
+        return result
